ab8a078b085a9536abfd4d28408c5c63707577fe
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2022-07-01 11:47:11 +0800

    update code

--- seckill-gateway/src/main/java/com/example/seckillgateway/SeckillGatewayApplication.java
+++ seckill-gateway/src/main/java/com/example/seckillgateway/SeckillGatewayApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class SeckillGatewayApplication {
 
     public static void main(String[] args) {
-        //不设置的化sentinel控制台不会显示网关相关流控选项
+        //不设置的话sentinel控制台不会显示网关相关流控选项
         System.setProperty("csp.sentinel.app.type", "1");
         SpringApplication.run(SeckillGatewayApplication.class, args);
     }
